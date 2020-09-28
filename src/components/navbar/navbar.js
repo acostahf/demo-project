0a5ecb375f85e59927567ec34595cca26bccb8e8
acostahf <acostahf4@gmail.com>
2020-09-28 15:03:34 -0500

    setup basic contact form

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,46 +1,58 @@
 import React, { Component } from "react";
 import {
-MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
-MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNav,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBFormInline,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
 } from "mdbreact";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 class Navbar extends Component {
-state = {
-  isOpen: false
-};
+  state = {
+    isOpen: false,
+  };
 
-toggleCollapse = () => {
-  this.setState({ isOpen: !this.state.isOpen });
-}
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
-render() {
-  return (
-    <>
-      <MDBNavbar color="indigo" dark expand="md">
-        <MDBNavbarBrand>
-        <MDBNavLink to="/"><strong className="white-text">Blog</strong></MDBNavLink>
-        </MDBNavbarBrand>
-        <MDBNavbarToggler onClick={this.toggleCollapse} />
-        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-          <MDBNavbarNav left>
-            <MDBNavItem active>
-            <MDBNavLink to="/about">About Us</MDBNavLink>
-            </MDBNavItem>
-           
-            <MDBNavItem>
-              <MDBNavLink to="/products">Products</MDBNavLink>
-            </MDBNavItem>
-           
-          </MDBNavbarNav>
-          <MDBNavbarNav right>
-          <MDBNavItem>
-              <MDBNavLink to="/contact">Contact</MDBNavLink>
-            </MDBNavItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBNavbar>
-    </>
+  render() {
+    return (
+      <>
+        <MDBNavbar color="info-color" dark expand="md">
+          <MDBNavbarBrand>
+            <MDBNavLink to="/">
+              <strong className="white-text">Blog</strong>
+            </MDBNavLink>
+          </MDBNavbarBrand>
+          <MDBNavbarToggler onClick={this.toggleCollapse} />
+          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+            <MDBNavbarNav className="d-flex justify-content-center">
+              <MDBNavItem active>
+                <MDBNavLink to="/about">About Us</MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavLink to="...">somthing </MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavLink to="/products">Menu</MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavLink to="/contact">Contact Us</MDBNavLink>
+              </MDBNavItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBNavbar>
+      </>
     );
   }
 }
--- src/pages/contactPage/contactPage.js
+++ src/pages/contactPage/contactPage.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import './contactPage.css';
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBBtn,
+  MDBIcon,
+  MDBInput,
+} from "mdbreact";
+import React from "react";
+import "./contactPage.css";
 
 function contact() {
   return (
-    <div>
-        <h1>contact</h1>
-    </div> 
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol>
+          <form className=" mt-4">
+            <p className="h1 text-center mb-4">Contact us</p>
+
+            <MDBInput
+              label="Your Name"
+              type="text"
+              id="defaultFormContactNameEx"
+              className="form-control"
+            />
+
+            <br />
+
+            <MDBInput
+              label="Your Email"
+              type="email"
+              id="defaultFormContactEmailEx"
+              className="form-control"
+            />
+            <br />
+            <MDBInput
+              label="Subject"
+              type="textarea"
+              id="defaultFormContactSubjectEx"
+              className="form-control"
+            />
+            <br />
+            <MDBInput
+              label="Your Message"
+              type="text"
+              id="defaultFormContactMessageEx"
+              className="form-control"
+              rows="3"
+            />
+            <div className="text-center mt-4">
+              <MDBBtn color="default" outline type="submit">
+                Send
+                <MDBIcon far icon="paper-plane" className="ml-2" />
+              </MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
 
