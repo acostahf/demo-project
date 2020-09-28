1b6d4d0b3019ba266c8bd15c915083acef9d9430
Andrew Smith <ajs3993@gmail.com>
2020-09-28 10:02:55 -0500

    navbar and pages

---
+++ src/pages/productsPage/productsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './productsPage.css';
+
+function Products() {
+  return (
+    <div>
+        <h1>products</h1>
+    </div> 
+  );
+}
+
+export default Products;
