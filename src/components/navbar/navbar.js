3ac077eb09246c836d08307ede24f1fd53eb773d
acostahf <acostahf4@gmail.com>
2020-09-29 16:33:20 -0500

    change the navBar to a image

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,58 +1,106 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   MDBNavbar,
   MDBNavbarBrand,
-  MDBNav,
   MDBNavbarNav,
-  MDBNavItem,
-  MDBNavLink,
   MDBNavbarToggler,
   MDBCollapse,
-  MDBFormInline,
-  MDBDropdown,
-  MDBDropdownToggle,
-  MDBDropdownMenu,
-  MDBDropdownItem,
+  MDBNavItem,
+  MDBNavLink,
+  MDBContainer,
+  MDBMask,
+  MDBView,
+  MDBTypography,
 } from "mdbreact";
 import { BrowserRouter as Router } from "react-router-dom";
 
-class Navbar extends Component {
-  state = {
-    isOpen: false,
-  };
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
 
-  toggleCollapse = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
 
   render() {
     return (
-      <>
-        <MDBNavbar color="elegant-color" dark expand="md">
-          <MDBNavbarBrand>
-            <MDBNavLink to="/">
-              <strong className="white-text">Blog</strong>
-            </MDBNavLink>
-          </MDBNavbarBrand>
-          <MDBNavbarToggler onClick={this.toggleCollapse} />
-          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-            <MDBNavbarNav className="d-flex justify-content-center">
-              <MDBNavItem active>
-                <MDBNavLink to="/about">About Us</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink to="...">somthing </MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink to="/products">Menu</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink to="/contact">Contact Us</MDBNavLink>
-              </MDBNavItem>
-            </MDBNavbarNav>
-          </MDBCollapse>
-        </MDBNavbar>
-      </>
+      <div>
+        <header>
+          <Router>
+            <MDBNavbar
+              color="elegant-color"
+              fixed="top"
+              dark
+              expand="md"
+              scrolling
+              transparent
+            >
+              <MDBNavbarBrand href="/">
+                <strong>Navbar</strong>
+              </MDBNavbarBrand>
+              {!this.state.isWideEnough && (
+                <MDBNavbarToggler onClick={this.onClick} />
+              )}
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="/product">Menu</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/about">About us</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/contact">Contact</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </Router>
+
+          <MDBView src="https://imgur.com/Pp9umSP.jpg">
+            <MDBMask
+              overlay="purple-light"
+              className="flex-center flex-column text-white text-center"
+            >
+              <MDBTypography tag="h1" variant="display-1">
+                Welcome!
+              </MDBTypography>
+              <MDBTypography tag="h3" variant="display-3">
+                We are a local food truck in the heart of Austin, Texas
+              </MDBTypography>
+            </MDBMask>
+          </MDBView>
+        </header>
+
+        <main>
+          <MDBContainer className="text-center my-5">
+            <p align="justify">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </MDBContainer>
+        </main>
+      </div>
     );
   }
 }
